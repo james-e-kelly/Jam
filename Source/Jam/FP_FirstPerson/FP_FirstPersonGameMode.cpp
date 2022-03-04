111c786e980be2d882ec72d1a419ea8e3459a564
James Kelly <jameskelly21699@gmail.com>
2022-03-04 22:26:13 +0000

    Comment out the annoying first person stuff

--- Source/Jam/FP_FirstPerson/FP_FirstPersonGameMode.cpp
+++ Source/Jam/FP_FirstPerson/FP_FirstPersonGameMode.cpp
@@ -8,8 +8,8 @@
 AFP_FirstPersonGameMode::AFP_FirstPersonGameMode()
 {
 	// Set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
-	DefaultPawnClass = PlayerPawnClassFinder.Class;
+//	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+//	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// Use our custom HUD class
 	HUDClass = AFP_FirstPersonHUD::StaticClass();
--- Source/Jam/FP_FirstPerson/FP_FirstPersonHUD.cpp
+++ Source/Jam/FP_FirstPerson/FP_FirstPersonHUD.cpp
@@ -10,8 +10,8 @@
 AFP_FirstPersonHUD::AFP_FirstPersonHUD()
 {
 	// Set the crosshair texture
-	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/FirstPerson/Textures/FirstPersonCrosshair"));
-	CrosshairTex = CrosshairTexObj.Object;
+//	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/FirstPerson/Textures/FirstPersonCrosshair"));
+//	CrosshairTex = CrosshairTexObj.Object;
 }
 
 /** This method draws a very simple crosshair */
@@ -19,15 +19,15 @@ void AFP_FirstPersonHUD::DrawHUD()
 {
 	Super::DrawHUD();
 
-	// Find center of the Canvas
-	const FVector2D Center(Canvas->ClipX * 0.5f, Canvas->ClipY * 0.5f);
-
-	// Offset by half the texture's dimensions so that the center of the texture aligns with the center of the Canvas
-	const FVector2D CrosshairDrawPosition( (Center.X - (CrosshairTex->GetSurfaceWidth() * 0.5)),
-										   (Center.Y - (CrosshairTex->GetSurfaceHeight() * 0.5f)) );
-
-	// Draw the crosshair
-	FCanvasTileItem TileItem( CrosshairDrawPosition, CrosshairTex->Resource, FLinearColor::White);
-	TileItem.BlendMode = SE_BLEND_Translucent;
-	Canvas->DrawItem( TileItem );
+//	// Find center of the Canvas
+//	const FVector2D Center(Canvas->ClipX * 0.5f, Canvas->ClipY * 0.5f);
+//
+//	// Offset by half the texture's dimensions so that the center of the texture aligns with the center of the Canvas
+//	const FVector2D CrosshairDrawPosition( (Center.X - (CrosshairTex->GetSurfaceWidth() * 0.5)),
+//										   (Center.Y - (CrosshairTex->GetSurfaceHeight() * 0.5f)) );
+//
+//	// Draw the crosshair
+//	FCanvasTileItem TileItem( CrosshairDrawPosition, CrosshairTex->Resource, FLinearColor::White);
+//	TileItem.BlendMode = SE_BLEND_Translucent;
+//	Canvas->DrawItem( TileItem );
 }
