57803ca30a6917d839abf80cd39d35a59bc04390
James Kelly <jameskelly21699@gmail.com>
2022-03-05 20:10:21 +0000

    Add music stuff to player controller

--- Source/Jam/JamPlayerController.cpp
+++ Source/Jam/JamPlayerController.cpp
@@ -3,3 +3,35 @@
 
 #include "JamPlayerController.h"
 
+void AJamPlayerController::PlayMenuMusic()
+{
+    StopAndReleaseEvent(GameplayMusicInstance);
+    PlayEvent(MenuMusic);
+}
+
+void AJamPlayerController::PlayGameplayMusic()
+{
+    StopAndReleaseEvent(MenuMusicInstance);
+    PlayEvent(GameplayMusic);
+}
+
+FMOD::Studio::EventInstance* AJamPlayerController::PlayEvent(UFMODEvent* Event)
+{
+    FFMODEventInstance PlayingInstance = UFMODBlueprintStatics::PlayEvent2D(this, Event, false);
+    
+    if (PlayingInstance.Instance != nullptr)
+    {
+        PlayingInstance.Instance->start();
+    }
+    
+    return PlayingInstance.Instance;
+}
+
+void AJamPlayerController::StopAndReleaseEvent(FMOD::Studio::EventInstance* Instance)
+{
+    if (Instance)
+    {
+        Instance->stop(FMOD_STUDIO_STOP_ALLOWFADEOUT);
+        Instance->release();
+    }
+}
--- Source/Jam/JamPlayerController.h
+++ Source/Jam/JamPlayerController.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
+#include "FMODBlueprintStatics.h"
 #include "JamPlayerController.generated.h"
 
 /**
@@ -13,5 +14,29 @@ UCLASS()
 class JAM_API AJamPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+    
+protected:
 	
+    UPROPERTY(EditAnywhere, BlueprintReadOnly)
+    UFMODEvent* MenuMusic;
+    
+    UPROPERTY(EditAnywhere, BlueprintReadOnly)
+    UFMODEvent* GameplayMusic;
+    
+public:
+    
+    UFUNCTION(BlueprintCallable)
+    void PlayMenuMusic();
+    
+    UFUNCTION(BlueprintCallable)
+    void PlayGameplayMusic();
+    
+private:
+    
+    FMOD::Studio::EventInstance* PlayEvent(UFMODEvent* Event);
+    
+    void StopAndReleaseEvent(FMOD::Studio::EventInstance* Instance);
+    
+    FMOD::Studio::EventInstance* MenuMusicInstance;
+    FMOD::Studio::EventInstance* GameplayMusicInstance;
 };
